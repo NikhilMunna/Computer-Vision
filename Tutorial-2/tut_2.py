e4eb401d730d59798757dbe5bbe05a7e4e22defe
NikhilMunna <nikhil.munna001@gmail.com>
2019-06-11 23:53:11 +0530

    removed repetitions

--- Tutorial-2/tut_2.py
+++ Tutorial-2/tut_2.py
@@ -5,10 +5,7 @@ import argparse
 import imutils
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
+
 
 class Tetris():
 
