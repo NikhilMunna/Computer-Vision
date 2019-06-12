3cff8c636b01528af7ce12a008f7ad921086c6e6
NikhilMunna <nikhil.munna001@gmail.com>
2019-06-12 19:40:36 +0530

    removed comments

--- Document-Scanner/scan.py
+++ Document-Scanner/scan.py
@@ -26,7 +26,6 @@ class Scanner():
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
         edged = cv2.Canny(gray, 75, 200)
 
-        # show the original image and the edge detected image
         print("STEP 1: Edge Detection")
         cv2.imshow("Image", image)
         cv2.imshow("Edged", edged)
@@ -39,14 +38,12 @@ class Scanner():
         cnts = imutils.grab_contours(cnts)
         cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
-        # loop over the contours
         for c in cnts:
-            # approximate the contour
+      
             peri = cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-            # if our approximated contour has four points, then we
-            # can assume that we have found our screen
+      
             if len(approx) == 4:
                 screenCnt = approx
                 break
@@ -61,17 +58,15 @@ class Scanner():
         cv2.destroyAllWindows()
         ratio = orig.shape[0] / 500.0
         
-        # apply the four point transform to obtain a top-down
-        # view of the original image
+      
         warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 
-        # convert the warped image to grayscale, then threshold it
-        # to give it that 'black and white' paper effect
+      
         warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
         T = threshold_local(warped, 11, offset = 10, method = "gaussian")
         warped = (warped > T).astype("uint8") * 255
 
-        # show the original and scanned images
+      
         print("STEP 3: Apply perspective transform")
         cv2.imshow("Original", imutils.resize(orig, height = 650))
         cv2.imshow("Scanned", imutils.resize(warped, height = 650))
