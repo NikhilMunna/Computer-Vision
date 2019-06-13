ceeecd210d8c310f89600e2ce135cb6a10c62309
NikhilMunna <nikhil.munna001@gmail.com>
2019-06-13 11:33:03 +0530

    removed comments

--- Object_size/object-size.py
+++ Object_size/object-size.py
@@ -1,7 +1,7 @@
 # USAGE
-# python object_size.py --image images/example_01.png --width 0.955
-# python object_size.py --image images/example_02.png --width 0.955
-# python object_size.py --image images/example_03.png --width 3.5
+# python object-size.py --image images/example_01.png --width 0.955
+# python object-size.py --image images/example_02.png --width 0.955
+# python object-size.py --image images/example_03.png --width 3.5
 
 from scipy.spatial import distance as dist
 from imutils import perspective
@@ -47,75 +47,54 @@ class Size():
         cnts = self.find_and_sort_contours()
         pixelsPerMetric = None
         for c in cnts:
-            # if the contour is not sufficiently large, ignore it
             if cv2.contourArea(c) < 100:
                 continue
 
-            # compute the rotated bounding box of the contour
-            orig = image.copy()
+            original = image.copy()
             box = cv2.minAreaRect(c)
             box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
             box = np.array(box, dtype="int")
 
-            # order the points in the contour such that they appear
-            # in top-left, top-right, bottom-right, and bottom-left
-            # order, then draw the outline of the rotated bounding
-            # box
             box = perspective.order_points(box)
-            cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+            cv2.drawContours(original, [box.astype("int")], -1, (0, 255, 0), 2)
 
-            # loop over the original points and draw them
             for (x, y) in box:
-                cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+                cv2.circle(original, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-            # unpack the ordered bounding box, then compute the midpoint
-            # between the top-left and top-right coordinates, followed by
-            # the midpoint between bottom-left and bottom-right coordinates
             (tl, tr, br, bl) = box
             (tltrX, tltrY) = midpoint(tl, tr)
             (blbrX, blbrY) = midpoint(bl, br)
 
-            # compute the midpoint between the top-left and top-right points,
-            # followed by the midpoint between the top-righ and bottom-right
             (tlblX, tlblY) = midpoint(tl, bl)
             (trbrX, trbrY) = midpoint(tr, br)
 
-            # draw the midpoints on the image
-            cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-            cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-            cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-            cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(original, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+            cv2.circle(original, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(original, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+            cv2.circle(original, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-            # draw lines between the midpoints
-            cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+            cv2.line(original, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
                 (255, 0, 255), 2)
-            cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+            cv2.line(original, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
                 (255, 0, 255), 2)
 
-            # compute the Euclidean distance between the midpoints
             dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
             dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-            # if the pixels per metric has not been initialized, then
-            # compute it as the ratio of pixels to supplied metric
-            # (in this case, inches)
             if pixelsPerMetric is None:
                 pixelsPerMetric = dB / args["width"]
 
-            # compute the size of the object
             dimA = dA / pixelsPerMetric
             dimB = dB / pixelsPerMetric
 
-            # draw the object sizes on the image
-            cv2.putText(orig, "{:.1f}in".format(dimA),
+            cv2.putText(original, "{:.1f}in".format(dimA),
                 (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
                 0.65, (255, 255, 255), 2)
-            cv2.putText(orig, "{:.1f}in".format(dimB),
+            cv2.putText(original, "{:.1f}in".format(dimB),
                 (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
                 0.65, (255, 255, 255), 2)
 
-            # show the output image
-            cv2.imshow("Image", orig)
+            cv2.imshow("Image", original)
             cv2.waitKey(0)
 
     
